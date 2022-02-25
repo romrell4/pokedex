d5b67c3b104ca496e020e97b47a11452d9828855
Mark Roth <mark_roth@byu.edu>
2022-02-25 10:36:15 -0700

    removed unnesessary parcelable

--- app/src/main/java/com/example/pokedex/domain/Pokemon.kt
+++ app/src/main/java/com/example/pokedex/domain/Pokemon.kt
@@ -1,9 +1,5 @@
 package com.example.pokedex.domain
 
-import android.os.Parcelable
-import kotlinx.parcelize.Parcelize
-
-@Parcelize
 data class Pokemon(
     val abilities: List<Ability>,
     val height: Int,
@@ -13,29 +9,25 @@ data class Pokemon(
     val stats: List<Stat>,
     val types: List<Type>,
     val weight: Int
-) : Parcelable {
-    @Parcelize
+) {
     data class Sprite(
         val frontDefault: String
-    ) : Parcelable
+    )
 
-    @Parcelize
     data class Stat(
         val baseStat: Int,
         val effort: Int,
         val stat: ListItem
-    ) : Parcelable
+    )
 
-    @Parcelize
     data class Ability(
         val ability: ListItem,
         val isHidden: Boolean,
         val slot: Int
-    ) : Parcelable
+    )
 
-    @Parcelize
     data class Type(
         val slot: Int,
         val type: ListItem
-    ) : Parcelable
+    )
 }
