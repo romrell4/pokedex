b0ce12532dd521f67bc9c77fb6b9f11dac9d0ec4
Mark Roth <mark_roth@byu.edu>
2022-02-25 13:05:07 -0700

    finished use-case and moved on to viewmodel

--- app/src/main/java/com/example/pokedex/domain/PokemonList.kt
+++ app/src/main/java/com/example/pokedex/domain/PokemonList.kt
@@ -1,8 +1,12 @@
 package com.example.pokedex.domain
 
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
 data class PokemonList(
     val count: Int,
     val next: String?,
     val previous: String?,
     val results: List<ListItem>
-)
+) : Parcelable
--- app/src/main/java/com/example/pokedex/usecase/GetAllPokemonUseCase.kt
+++ app/src/main/java/com/example/pokedex/usecase/GetAllPokemonUseCase.kt
@@ -1,8 +1,15 @@
 package com.example.pokedex.usecase
 
-interface GetAllPokemonUseCase {
+import com.example.pokedex.domain.PokemonList
+import com.example.pokedex.repo.PokedexRepository
+import retrofit2.await
 
+interface GetAllPokemonUseCase {
+    suspend fun execute(): PokemonList
 }
 
-class GetAllPokemonUseCaseImpl : GetAllPokemonUseCase {
+class GetAllPokemonUseCaseImpl(
+    private val repo: PokedexRepository
+) : GetAllPokemonUseCase {
+    override suspend fun execute() = repo.getAllPokemon().await()
 }
